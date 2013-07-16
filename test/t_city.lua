9098b980d293ccdc2d42c668d40c8df6640846fa
Chris Aubuchon <Chris.Aubuchon@gmail.com>
2013-07-16 12:57:25 -0500

    Added some tests

---
+++ test/t_city.lua
@@ -0,0 +1,6 @@
+require "bt1test"
+require "city"
+
+local skara = city:new("skara")
+print(skara.title)
+print(skara:isCity())
---
+++ test/t_dun.lua
@@ -0,0 +1,6 @@
+require "bt1test"
+require "dataio"
+require "dun"
+
+
+local d = dun:new("cellars", 1, 0, 0, "north")
